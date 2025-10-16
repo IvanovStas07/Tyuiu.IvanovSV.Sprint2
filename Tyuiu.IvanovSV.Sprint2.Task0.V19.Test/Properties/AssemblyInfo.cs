2c11efd59fc741ffb9fd1e8feaa1e4f719313d19
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-10-17 04:31:29 +0500

    Add project files.

---
+++ Tyuiu.IvanovSV.Sprint2.Task0.V19.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvanovSV.Sprint2.Task0.V19.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvanovSV.Sprint2.Task0.V19.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b4cf537f-de77-4570-827b-f0ad9f790c65")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.IvanovSV.Sprint2.Task0.V19/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IvanovSV.Sprint2.Task0.V19.Lib;
+
+namespace Tyuiu.IvanovSV.Sprint2.Task0.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт 1 | Выполнил: Иванов С В | ИСТНб-25-1";
+            Console.WriteLine(new string('*', 75));
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Задание #0                                                             *");
+            Console.WriteLine("* Вариант #19                                                            *");
+            Console.WriteLine("* Выполнил: Иванов С В | ИСТНб-25-1                                       *");
+            Console.WriteLine(new string('*', 75));
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=, последовательность операций не должна нарушаться) и арифметических выражений,  *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив): (True, True, True, True, True, True), при x = 105, y = 177                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine(new string('*', 75));
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine(new string('*', 75));
+            int x = 105;
+            int y = 177;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+            Console.WriteLine("РЕЗУЛЬТАТ");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
