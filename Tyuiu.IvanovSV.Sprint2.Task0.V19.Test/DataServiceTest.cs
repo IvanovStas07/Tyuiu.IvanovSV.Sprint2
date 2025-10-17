e16ce47e1c992e861f924375c03b38b4d63d3f76
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-10-17 23:17:50 +0500

    Task0

---
+++ Tyuiu.IvanovSV.Sprint2.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.IvanovSV.Sprint2.Task0.V19.Lib;
+
+namespace Tyuiu.IvanovSV.Sprint2.Task0.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 105;
+            int y = 177;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { true, true, true, true, true, true };
+
+            CollectionAssert.AreEqual(res, wait);
+        }
+    }
+}
+
+
+
