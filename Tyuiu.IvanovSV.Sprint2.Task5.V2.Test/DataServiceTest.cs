2eda9c11c7a59bea57b496302b96cc403759aeb0
IvanovStas07 <Tyuiu.IvanovSV@outlook.com>
2025-10-18 00:11:42 +0500

    Task5

---
+++ Tyuiu.IvanovSV.Sprint2.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.IvanovSV.Sprint2.Task5.V2.Lib;
+
+namespace Tyuiu.IvanovSV.Sprint2.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestWinterMonth()
+        {
+            DataService ds = new DataService();
+            string result = ds.FindMonthSeason(1);
+            Assert.AreEqual("Зима", result);
+        }
+
+        [TestMethod]
+        public void TestSummerMonth()
+        {
+            DataService ds = new DataService();
+            string result = ds.FindMonthSeason(7);
+            Assert.AreEqual("Лето", result);
+        }
+
+        [TestMethod]
+        public void TestInvalidMonth()
+        {
+            DataService ds = new DataService();
+            string result = ds.FindMonthSeason(13);
+            Assert.AreEqual("Ошибка: нет такого месяца", result);
+
+
+        }
+    }
+}
---
+++ Tyuiu.IvanovSV.Sprint2.Task5.V2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.IvanovSV.Sprint2.Task5.V2.Lib;
+namespace Tyuiu.IvanovSV.Sprint2.Task5.V2
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "Спринт #2 | Выполнил: Иванов С. В. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #2                                                              *");
+        Console.WriteLine("* Выполнил: Иванов С. В. | ИСТНб-25-1                                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* По номеру месяца определить пору года: Зима, Весна, Лето, Осень.        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.Write("Введите номер месяца (1–12): ");
+        int month = Convert.ToInt32(Console.ReadLine());
+
+        DataService ds = new DataService();
+        string season = ds.FindMonthSeason(month);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Пора года: " + season);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
+}
